be17689a885b0c897d86137d3dd418f2ce5612c9
wrench1997 <ljl260435988@gmail.com>
2025-09-03 15:02:38 +0800

    change ip address

--- install_mobsf.sh
+++ install_mobsf.sh
@@ -125,7 +125,7 @@ python3 manage.py createsuperuser --noinput --email ""
 set -e
 python3 manage.py create_roles
 
-exec gunicorn -b 0.0.0.0:8000 "mobsf.MobSF.wsgi:application" --workers=1 --threads=10 --timeout=3600 \
+exec gunicorn -b 127.0.0.1:8000 "mobsf.MobSF.wsgi:application" --workers=1 --threads=10 --timeout=3600 \
     --worker-tmp-dir=/dev/shm --log-level=citical --log-file=- --access-logfile=- --error-logfile=- --capture-output
 EOF
 
